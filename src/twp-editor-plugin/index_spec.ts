9aa6b5885395cd4905cde64a42c513aa72aa3a34
Heather Roberts <hroberts@atlassian.com>
2020-06-02 13:54:28 +1000

    Add unit test for checking plugin's index.tsx file is generated

--- src/twp-editor-plugin/index_spec.ts
+++ src/twp-editor-plugin/index_spec.ts
@@ -1,18 +1,26 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import {
+  SchematicTestRunner,
+  UnitTestTree,
+} from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('twp-editor-plugin', () => {
-  it('works', () => {
+  const runSchematic = (name: string, usePluginState = false): UnitTestTree => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     const tree = runner.runSchematic(
       'twp-editor-plugin',
-      { name: 'my awesome plugin', usePluginState: false },
+      { name, usePluginState },
       Tree.empty()
     );
+    return tree;
+  };
 
-    expect(tree.files).toEqual(['/my-awesome-plugin/index.tsx']);
+  it('generates index file', () => {
+    expect(runSchematic('plugin').files).toContain(
+      '/packages/editor/editor-core/src/plugins/plugin/index.tsx'
+    );
   });
 });

3c7bfdabcb157da84eff632b73002e2985d1b1e3
Heather Roberts <hroberts@atlassian.com>
2020-06-02 12:06:49 +1000

    Use jest test runner instead of jasmine

--- src/twp-editor-plugin/index_spec.ts
+++ src/twp-editor-plugin/index_spec.ts
@@ -2,15 +2,17 @@ import { Tree } from '@angular-devkit/schematics';
 import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
-
 const collectionPath = path.join(__dirname, '../collection.json');
-
 
 describe('twp-editor-plugin', () => {
   it('works', () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = runner.runSchematic('twp-editor-plugin', {}, Tree.empty());
+    const tree = runner.runSchematic(
+      'twp-editor-plugin',
+      { name: 'my awesome plugin', usePluginState: false },
+      Tree.empty()
+    );
 
-    expect(tree.files).toEqual([]);
+    expect(tree.files).toEqual(['/my-awesome-plugin/index.tsx']);
   });
 });

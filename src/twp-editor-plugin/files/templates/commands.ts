bedb96bcdae5f40fc1cbf05ca4dd075dd6e89d3b
Tim Hor <timothy.hor@outlook.com>
2020-06-05 16:22:58 +1000

    Remove extra newlines from generated commands.ts

--- src/twp-editor-plugin/files/templates/commands.ts
+++ src/twp-editor-plugin/files/templates/commands.ts
@@ -1,10 +1,8 @@
-<% if (usePluginState) { %>
-import { createCommand } from './pm-plugins/plugin-factory';
+<% if (usePluginState) { %>import { createCommand } from './pm-plugins/plugin-factory';
 import { <%= classify(name) %>ActionTypes } from './actions';
 
 export const someCommand = () =>
   createCommand({
     type: <%= classify(name) %>ActionTypes.SOME_ACTION,
   });
-
 <% } %>

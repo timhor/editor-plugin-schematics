80e5d24a49dabcc8375a5c44ae9277548bf16363
Tim Hor <timothy.hor@outlook.com>
2020-06-05 10:54:08 +1000

    Update sample create-plugins-list to better reflect the actual file

--- src/twp-editor-plugin/files/manual-testing/create-plugins-list.ts
+++ src/twp-editor-plugin/files/manual-testing/create-plugins-list.ts
@@ -1,9 +1,27 @@
 // simulates packages/editor/editor-core/src/create-editor/create-plugins-list.ts
 
+import { CreateUIAnalyticsEvent } from '@atlaskit/analytics-next';
+import { EditorPlugin, EditorProps } from '../types';
 import {
   existingPlugin,
 } from '../plugins';
+import { isFullPage as fullPageCheck } from '../utils/is-full-page';
+import { ScrollGutterPluginOptions } from '../plugins/base/pm-plugins/scroll-gutter';
+import { createFeatureFlagsFromProps } from '../plugins/feature-flags-context/feature-flags-from-props';
+import { PrivateCollabEditOptions } from '../plugins/collab-edit/types';
+import { BlockTypePluginOptions } from '../plugins/block-type/types';
 
+export function getDefaultPluginsList(props: EditorProps): EditorPlugin[] {
+  return [];
+}
+
+function getScrollGutterOptions(
+  props: EditorProps,
+): ScrollGutterPluginOptions | undefined {}
+
+/**
+ * Maps EditorProps to EditorPlugins
+ */
 export default function createPluginsList(
   props: EditorProps,
   prevProps?: EditorProps,

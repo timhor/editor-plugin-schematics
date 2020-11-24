422e4831f037c4f917fa093244010641c9d93d1b
Tim Hor <timothy.hor@outlook.com>
2020-11-24 14:05:13 +1100

    Use named import for React

--- src/twp-editor-plugin/files/templates/index.tsx
+++ src/twp-editor-plugin/files/templates/index.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { EditorPlugin } from '../../types';
 import { createPlugin } from './pm-plugins/main';<% if (useKeymap) { %>
 import keymapPlugin from './pm-plugins/keymap';<% } %><% if (useInputRules) { %>
